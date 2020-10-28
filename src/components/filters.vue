09b3ccd52950c6fb471e61af5da0c31146569546
Lisa Poncet <poncet.lisa@gmail.com>
2020-10-28 09:39:03 +0100

    struggle passing state to sibling

--- src/components/filters.vue
+++ src/components/filters.vue
@@ -1,21 +1,51 @@
 <template>
   <div class="filter-container">
     <button>reset filters</button>
-    <p>price max</p>
-    <input type="range" />
+    <hr />
+    <div class="price-filter">
+      <p>price max</p>
+      <div class="price-input">
+        <input
+          id="price-range"
+          @change="$emit('price-change', maxPrice)"
+          v-model.number="maxPrice"
+          type="range"
+          min="10"
+          max="60"
+          step="1"
+        />
+        <label for="price-range">{{ maxPrice }}€</label>
+      </div>
+    </div>
+    <hr />
+    <div class="color-filter">
+      <p>colors</p>
+      <div class="color-blocks">
+        <div class="color-block" :style="{ 'background-color': 'red' }"></div>
+        <div class="color-block" :style="{ 'background-color': 'blue' }"></div>
+        <div class="color-block" :style="{ 'background-color': 'green' }"></div>
+        <div class="color-block" :style="{ 'background-color': 'black' }"></div>
+      </div>
+    </div>
+    <hr />
     <p>brands</p>
-    <p>colors</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Filters",
-  components: {}
+  components: {},
+  data() {
+    console.log("hello");
+    return {
+      maxPrice: 10
+    };
+  }
 };
 </script>
 
-<style>
+<style scoped>
 .filter-container {
   margin: 0px 10px 0px 10px;
   padding: 10px;
@@ -23,5 +53,24 @@ export default {
   border: 1px solid black;
   flex-direction: column;
   align-items: flex-start;
+}
+
+p {
+  text-align: start;
+}
+
+.price-input {
+  display: flex;
+}
+
+.color-blocks {
+  display: flex;
+}
+
+.color-block {
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+  cursor: pointer;
 }
 </style>
--- src/components/products-grid.vue
+++ src/components/products-grid.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="grid">
-    <ProductCard v-for="(product, i) in products" :key="i" :product="product" />
+    <ProductCard
+      v-on:price-change="handlePriceRange"
+      v-for="(product, i) in products"
+      :key="i"
+      :product="product"
+    />
   </div>
 </template>
 
@@ -13,26 +18,42 @@ export default {
     ProductCard
   },
 
+  methods: {
+    handlePriceRange(maxPrice) {
+      console.log(maxPrice);
+    }
+  },
+
   data() {
     return {
       products: [
         {
           name: "blue tshirt",
           price: 45,
+          color: "blue",
           image:
             "https://i.pinimg.com/originals/06/14/28/0614281c43fda3ad87fa9a82371032c4.png"
         },
         {
           name: "red tshirt",
           price: 30,
+          color: "red",
           image:
             "https://i.pinimg.com/originals/33/ed/7f/33ed7f9cfde40c9ebc21f5d7ccf42e29.png"
         },
         {
           name: "green tshirt",
           price: 52,
+          color: "green",
           image:
             "https://i.pinimg.com/originals/a5/73/d8/a573d87027a882332ae8aff5f19da24a.png"
+        },
+        {
+          name: "black tshirt",
+          price: 38,
+          color: "black",
+          image:
+            "https://i.pinimg.com/originals/4e/be/50/4ebe50e2495b17a79c31e48a0e54883f.png"
         }
       ]
     };
